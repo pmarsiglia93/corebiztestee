b2ca3c6e4abde0b5e0f7ce287aaa389b0fd53d0c
Thays Silva Diab <thayspereirasilva2@gmail.com>
2021-04-29 11:35:00 -0300

    criar componente Newsletter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Carousel from "./components/Carousel";
+import Showcase from "./components/Showcase";
+import Newsletter from "./components/Newsletter";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import ShoppingCartContext from "./contexts/ShoppingCartContext";
+
+class App extends React.Component {
+  state = {
+    cart: [],
+    addProducts: (newProduct) => {
+      let updatedCart = [...this.state.cart, newProduct];
+
+      console.log([...this.state.cart, newProduct]);
+      this.setState({
+        cart: updatedCart,
+      });
+
+      window.localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+    },
+  };
+
+  componentDidMount() {
+    let cartItems = JSON.parse(window.localStorage.getItem("cartItems"));
+
+    if (cartItems != null) {
+      this.setState({
+        cart: [...cartItems],
+      });
+    }
+  }
+
+  render() {
+    return (
+      <ShoppingCartContext.Provider value={this.state}>
+        <div>
+          <Header />
+          <Carousel />
+          <Showcase />
+          <Newsletter />
+        </div>
+      </ShoppingCartContext.Provider>
+    );
+  }
 }
 
 export default App;

9bc56921923652a3cfd2c4751906c4dafef02c2d
Thays Silva Diab <thayspereirasilva2@gmail.com>
2021-04-30 12:11:29 -0300

    ajustar validação de e-mail

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -20,9 +20,9 @@ const Newsletter = () => {
   }
 
   function validateEmail(email) {
-    const re = /^(([^<>()\\]\\.,;:\s@"]+(\.[^<>()\\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  }
+}
   function resetEmail() {
     setNewsletterSuccess(false);
   }
